87b840148443f40203e31eccf489a25072836365
LucaLodii <luca.lodi.ll53@gmail.com>
2025-10-20 12:43:44 -0300

    fix: reference errors

--- ConsultaCep.Application/DTOs/ConsultarCepResponse.cs
+++ ConsultaCep.Application/DTOs/ConsultarCepResponse.cs
@@ -4,11 +4,11 @@ namespace ConsultaCep.Application.DTOs
 {
     public class ConsultarCepResponse
     {
-        public string? Cep { get; set; }
-        public string? Logradouro { get; set; }
-        public string? Localidade { get; set; }
-        public string? UF { get; set; }
-        public string? Bairro { get; set; }
-        public string? Complemento { get; set; }
+        public string Cep { get; set; }
+        public string Logradouro { get; set; }
+        public string Localidade { get; set; }
+        public string UF { get; set; }
+        public string Bairro { get; set; }
+        public string Complemento { get; set; }
     }
 }
--- ConsultaCep.Domain/Entities/Endereco.cs
+++ ConsultaCep.Domain/Entities/Endereco.cs
@@ -2,11 +2,11 @@ namespace ConsultaCep.Domain.Entities
 {
     public class Endereco   // Main entity of the domain, (Task 2)
     {
-        public string? Cep { get; set; }
-        public string? Logradouro { get; set; }
-        public string? Localidade { get; set; } // Its the same as Cidade
-        public string? UF { get; set; }
-        public string? Bairro { get; set; }
-        public string? Complemento { get; set; }
+        public string Cep { get; set; }
+        public string Logradouro { get; set; }
+        public string Localidade { get; set; } // Its the same as Cidade
+        public string UF { get; set; }
+        public string Bairro { get; set; }
+        public string Complemento { get; set; }
     }
 }
